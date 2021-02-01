880c00808da52fe1984755377c6667370e1fc089
Harry <harry.hancock@icloud.com>
2021-02-01 13:28:48 +0000

    generated test file

---
+++ tests/test_card_deck.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestCardDeck(TestCase):
+    def test_card(self):
+        self.fail()
+
+    def test_hand(self):
+        self.fail()
+
+    def test_player(self):
+        self.fail()
+
+    def test_card_deck(self):
+        self.fail()
+
+    def test_table(self):
+        self.fail()
