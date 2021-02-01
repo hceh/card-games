7716ff9e949349598b4b226c94a841a88213e714
Harry <harry.hancock@icloud.com>
2021-02-01 14:17:14 +0000

    card tests written

--- tests/test_card_deck.py
+++ tests/test_card_deck.py
@@ -1,18 +1,20 @@
 from unittest import TestCase
+from src.card_deck import *
 
 
-class TestCardDeck(TestCase):
-    def test_card(self):
-        self.fail()
+class TestCard(TestCase):
+    def test_card_creation(self):
+        self.assertEqual(Card('4h').card, '4❤')
+        self.assertEqual(Card('4H').card, '4❤')
+        self.assertEqual(Card('4❤').card, '4❤')
+        self.assertEqual(Card('10D').card, '10♦')
 
-    def test_hand(self):
-        self.fail()
+    def test_suit_attr(self):
+        self.assertEqual(Card('10D').suit, '♦')
+        self.assertEqual(Card('A♦').suit, '♦')
+        self.assertEqual(Card('JH').suit, '❤')
 
-    def test_player(self):
-        self.fail()
-
-    def test_card_deck(self):
-        self.fail()
-
-    def test_table(self):
-        self.fail()
+    def test_value_attr(self):
+        self.assertEqual(Card('10D').value, '10')
+        self.assertEqual(Card('QC').value, 'Q')
+        self.assertEqual(Card('AS').value, 'A')
