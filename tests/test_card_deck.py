395cbd0d00333602fbdf606e2b415b048cc6ff97
Harry <harry.hancock@icloud.com>
2021-02-01 14:44:06 +0000

    card deck tests planned

--- tests/test_card_deck.py
+++ tests/test_card_deck.py
@@ -18,3 +18,33 @@ class TestCard(TestCase):
         self.assertEqual(Card('10D').value, '10')
         self.assertEqual(Card('QC').value, 'Q')
         self.assertEqual(Card('AS').value, 'A')
+
+
+class TestCardDeck(TestCase):
+
+    def test_n_cards(self):
+        # todo:
+        #  basic deck = 52
+        #  other examples tested
+        self.fail()
+
+    def test_creation(self):
+        # todo:
+        #  standard pack
+        #  jokers
+        #  >1 decks
+        #  mix
+        self.fail()
+
+    def test_shuffle(self):
+        # todo:
+        #  compare slices of shuffled deck to initial deck, check diff
+        #  check set of before and after same
+        #  check n_cards same between two
+        self.fail()
+
+    def test_draw_card(self):
+        # todo: check that what is drawn is a card
+        #  card was in the deck
+        #  card is no longer in deck
+        self.fail()
